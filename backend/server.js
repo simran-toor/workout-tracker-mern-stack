ae87deabbf6cf5e49332aac5123c3a8fb8df7332
Simran Toor <simrankaurtoor@outlook.com>
2025-02-19 17:08:25 +0000

    mongoose installed, mongodb setup and inputed into .env, in server.js connected to db, listening for requests and catch error tested - working

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
 
 // express app 
 const app = express()
 const workoutRoutes = require('./routes/workouts')
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
@@ -14,10 +18,18 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 
+// connect to db 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+       // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        }) 
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+
 
 
