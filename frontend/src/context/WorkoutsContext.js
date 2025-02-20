fa851e27e25bc39c9261e4ec14c9ec316f28ee21
Simran Toor <simrankaurtoor@outlook.com>
2025-02-20 18:36:20 +0000

    WorkoutContexts context created that is used in function WorkoutsContextProvider which is used to wrap <App /> tag in index.js

---
+++ frontend/src/context/WorkoutsContext.js
@@ -0,0 +1,12 @@
+import { createContext } from 'react'
+
+export const WorkoutsContext = createContext()
+
+export const WorkoutsContextProvider = () => {
+
+    return (
+        <WorkoutsContext.Provider>
+            
+        </WorkoutsContext.Provider>
+    )
+}
