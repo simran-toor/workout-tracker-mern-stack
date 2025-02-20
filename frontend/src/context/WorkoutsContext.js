c60266fcc5ab5265b2fe967c5336ffe35079305e
Simran Toor <simrankaurtoor@outlook.com>
2025-02-20 19:20:17 +0000

    global state set up using react context

--- frontend/src/context/WorkoutsContext.js
+++ frontend/src/context/WorkoutsContext.js
@@ -1,12 +1,31 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
+
 
 export const WorkoutsContext = createContext()
 
-export const WorkoutsContextProvider = () => {
+export const workoutsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_WORKOUTS': 
+            return {
+                workouts: action.payload
+            }
+        case 'CREATE_WORKOUT':
+            return {
+                workouts: [action.payload, ...state.workouts]
+            }
+        default:
+            return state
+    }
+}
+
+export const WorkoutsContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(workoutsReducer, {
+        workouts: null
+    })
 
     return (
-        <WorkoutsContext.Provider>
-            
+        <WorkoutsContext.Provider value={{ state, dispatch  }}>
+            { children }
         </WorkoutsContext.Provider>
     )
 }
