5dd3156f679e222eaaae865bb47648701be924e5
Simran Toor <simrankaurtoor@outlook.com>
2025-02-20 17:47:20 +0000

    WorkoutDetails component created and used in Home.js to display workout details on homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react'
 
+// components
+import WorkoutDetails from '../components/WorkoutDetails'
+
 const Home = () => {
     const [workouts, setWorkouts] = useState(null)
 
@@ -21,7 +24,7 @@ const Home = () => {
         <div className="home">
             <div className="workouts">
                 {workouts && workouts.map((workout) => (
-                    <p key={workout._id}>{workout.title}</p>
+                    <WorkoutDetails key={workout._id} workout={workout} />
                 ))}
             </div>
         </div>
