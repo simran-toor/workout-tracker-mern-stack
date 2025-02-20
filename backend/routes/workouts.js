1f60b948479f8ba4de8d2b4d091b604bfe5e9e3f
Simran Toor <simrankaurtoor@outlook.com>
2025-02-20 15:15:38 +0000

    deleteWorkout and updateWorkout imported into workout.js and replaced placeholder json message in corresponding routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,6 +3,8 @@ const {
     getWorkouts,
     getWorkout,
     createWorkout,
+    deleteWorkout,
+    updateWorkout
 } = require('../controllers/workoutController')
 
 const router = express.Router()
@@ -17,14 +19,10 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 // DELETE a workout 
-router.delete('/:id', (req, res) => {
-    res.json({msg: 'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({msg: 'UPDATE a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router 
 
