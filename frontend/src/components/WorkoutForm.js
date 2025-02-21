ca02a1331e2e5aa9316818b50e894b36bb83ba4b
Simran Toor <simrankaurtoor@outlook.com>
2025-02-21 14:32:43 +0000

    error message updated to be more user friendly and conditional classNames added so if an input field is empty when submitting workout it will outline in red missing input fields

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -9,6 +9,7 @@ const WorkoutForm = () => {
     const [load, setLoad] = useState("")
     const [reps, setReps] = useState("")
     const [error, setError] = useState(null)
+    const [emptyFields, setEmptyFields] = useState([])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -26,44 +27,49 @@ const WorkoutForm = () => {
 
         if (!response.ok) {
             setError(json.error)
+            setEmptyFields(json.emptyFields)
         }
         if (response.ok) {
             setTitle("")
             setLoad("")
             setReps("")
             setError(null)
+            setEmptyFields([])
             console.log("new workout added", json)
             dispatch({ type: 'CREATE_WORKOUT', payload: json })
         }
     }
 
     return (
-        <form className="create" onSubmit={handleSubmit}>
+        <form className="create" onSubmit={ handleSubmit }>
             <h3>Add a New Workout</h3>
 
             <label>Exercise Title:</label>
             <input 
                 type="text"
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
+                onChange={ (e) => setTitle(e.target.value) }
+                value={ title }
+                className={ emptyFields.includes("title") ? "error" : "" }
             />
 
             <label>Load (in kg):</label>
             <input 
                 type="number"
-                onChange={(e) => setLoad(e.target.value)}
-                value={load}
+                onChange={ (e) => setLoad(e.target.value) }
+                value={ load }
+                className={ emptyFields.includes("load") ? "error" : "" }
             />
 
             <label>Reps:</label>
             <input 
                 type="number"
-                onChange={(e) => setReps(e.target.value)}
+                onChange={ (e) => setReps(e.target.value) }
                 value={reps}
+                className={ emptyFields.includes("reps") ? "error" : "" }
             />
 
             <button>Add Workout</button>
-            {error && <div className="error">{error}</div>}
+            { error && <div className="error">{error}</div> }
         </form>
     )
 }
